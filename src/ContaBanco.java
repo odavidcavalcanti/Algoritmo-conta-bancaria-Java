d1726775fa666ced1ca91d088807f773390d98bf
David Cavalcanti <212892555+deeolic@users.noreply.github.com.>
2025-07-23 16:23:36 -0300

    feat: Métodos getters e setters criados

--- src/ContaBanco.java
+++ src/ContaBanco.java
@@ -4,4 +4,45 @@ public class ContaBanco {
     private String donoConta;
     private float saldoConta;
     private boolean statusConta;
+
+    public int getNumConta() {
+        return this.numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public char getTipoConta() {
+        return this.tipoConta;
+    }
+
+    public void setTipoConta(char tipoConta) {
+        this.tipoConta = tipoConta;
+    }
+
+    public String getDonoConta() {
+        return this.donoConta;
+    }
+
+    public void setDonoConta(String donoConta) {
+        this.donoConta = donoConta;
+    }
+
+    public float getSaldoConta() {
+        return this.saldoConta;
+    }
+
+    public void setSaldoConta(float saldoConta) {
+        this.saldoConta = saldoConta;
+    }
+
+    public boolean getStatusConta() {
+        return statusConta;
+    }
+
+    public void setStatusConta(boolean statusConta) {
+        this.statusConta = statusConta;
+    }
+
 }
